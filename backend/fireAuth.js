fe854ddfdff0f88e945906b7435dee5e172aadcd
justin ta <justinta@justins-MBP.lan>
2024-04-05 19:23:53 -0700

    Google Login now adds to Database

--- backend/fireAuth.js
+++ backend/fireAuth.js
@@ -3,7 +3,7 @@
 
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
 import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
-
+import { getFirestore, doc, setDoc, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCDHx8Nf9PVIsSWulOZLnLqUha9zLVExg0",
@@ -22,23 +22,58 @@ const auth = getAuth(app);
 auth.languageCode = 'en';
 const provider = new GoogleAuthProvider();
 
+//Google login button, after loggin in, stores user data into firebase
 const googleLogin = document.getElementById("google-login-btn");
 googleLogin.addEventListener("click",function(){
-  console.log('Hello, world!');
+  
   signInWithPopup(auth, provider)
   .then((result) => {
    
+    /*
+      Add functionality that after logging in, changes homepage and updates to reflect logged in info
+    */
+    
     const credential = GoogleAuthProvider.credentialFromResult(result);
-    const user = result.user;
-    console.log(user);
+    const db = getFirestore();
+
+    
+    const user = result.user; //Get user data into user variable
+    console.log(user);  //Print user data to console
+
+    const userData = {
+      displayName: user.displayName,
+      photo: user.photoURL,
+
+      followers: [],
+      following: [],
+      savedRecipes: [],
+      createdRecipes: [],
+
+    };
+
     //window.location.href = "../logged.html"     Make so after loggin in, send back to home, but with more functionalities (later)
+    const colRef = collection(db, 'users');
+    addDoc(colRef, userData)
+      .then((docRef) => {
+        console.log('Document written with ID: ', docRef.id);
+    })
+  .catch((error) => {
+    console.error('Error adding document: ', error);
+  });
+
+    
   }).catch((error) => {
     
     const errorCode = error.code;
     const errorMessage = error.message;
+    console.log('Error Code: ' + errorCode);
+    console.log('Error Message: ' + errorMessage)
+    console.error('Google Login Error');
     
     
   });
 })
+
+
 
 
