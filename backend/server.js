06c255768e202d114b306eaf07a1c7abaf5527fa
Simon Ly <50601011+SiLyGoose@users.noreply.github.com>
2024-05-08 01:17:59 -0700

    Cleaned up API

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
+const express = require("express");
+const path = require("path");
+const app = express();
 
-const express = require("express")
-const path = require('path')
-const app = express()
-
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Router is in users.js
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-const userRouter = require('./routes/users')
+const userRouter = require("./routes/users");
 
-app.use('/users', userRouter)
+app.use("/users", userRouter);
 
-app.listen(3000)
+app.listen(3000, () => {
+	console.log("Server listening to port 3000");
+});
